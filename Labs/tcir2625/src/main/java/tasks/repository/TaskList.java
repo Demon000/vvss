20fb6454fc4c7a25ef2b0783f1c0aac07e2f83b0
Cosmin Tanislav <demonsingur@gmail.com>
2021-03-18 22:40:46 +0200

    tasks: remove unused incoming method from TaskList

--- Labs/tcir2625/src/main/java/tasks/repository/TaskList.java
+++ Labs/tcir2625/src/main/java/tasks/repository/TaskList.java
@@ -15,25 +15,4 @@ public abstract class TaskList implements Iterable<Task>, Serializable  {
     public abstract List<Task> getAll();
 
     public abstract Iterator<Task> iterator();
-
-    public TaskList incoming(Date from, Date to){
-        TaskList incomingTasks;
-        if (this instanceof ArrayTaskList){
-            incomingTasks = new ArrayTaskList();
-        }
-        else {
-            incomingTasks = new LinkedTaskList();
-        }
-
-        for(int i = 0; i < this.size(); i++){
-            if(getTask(i).nextTimeAfter(from) != null && getTask(i).nextTimeAfter(from).before(to)){
-                incomingTasks.add(getTask(i));
-                System.out.println(getTask(i).getDescription());
-            }
-        }
-        return incomingTasks;
-    }
-
-
-
 }
